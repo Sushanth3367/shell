001d5e39c0c9d3a706e3dcbe529388db1eb7cfac
Sushanth3367 <132801834+Sushanth3367@users.noreply.github.com>
2023-05-27 20:46:02 +0530

    config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 w = open('config.json', 'w+')
 w.write('{')
 w.write('\n')
-w.write('    "authToken": "'+os.getenv('Bot Token : 6275013194:AAFnfjlU9QjF2YPL3aS1xKtgwoNUqQdbALQ')+'",')
+w.write('    "authToken": "'+os.getenv('6275013194:AAFnfjlU9QjF2YPL3aS1xKtgwoNUqQdbALQ')+'",')
 w.write('\n')
 w.write('    "owner": '+os.getenv('1451257129'))
 w.write('\n')
